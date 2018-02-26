80ccdcea17227c618e45f0a92696f01aadebf2e7
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-02-25 20:10:31 -0500

    Using Logging

--- src/com/prasant/spring/mvc/controller/HomeController.java
+++ src/com/prasant/spring/mvc/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.prasant.spring.mvc.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
+	private static Logger LOGGER = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
-	public String showHome(Model model) {		
+	public String showHome(Model model) {
+		LOGGER.info("Showing home page.");
 		return "home";
 	}
 	
